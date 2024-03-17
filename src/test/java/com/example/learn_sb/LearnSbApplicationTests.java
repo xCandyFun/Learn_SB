7ba07c133c50507aacd07d2ddb6695fbf662d3aa
Simon T <simtho0624@gmail.com>
2024-03-17 18:04:53 +0100

    Initial commit

---
+++ src/test/java/com/example/learn_sb/LearnSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
